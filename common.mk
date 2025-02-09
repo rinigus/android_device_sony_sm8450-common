c45026f15ec4b93673360b1e093bda64e8ea15ed
Rinigus <rinigus.git@gmail.com>
2025-02-09 13:21:15 +0200

    sm8450: Drop missing android.hardware.graphics.mapper@3.0

--- common.mk
+++ common.mk
@@ -167,7 +167,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     libqdMetaData \
     libtinyxml \
-    android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
