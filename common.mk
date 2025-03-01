a37b591ee0f0b9bba7935829b8dbf21eafd4ab56
Rinigus <rinigus.git@gmail.com>
2025-03-01 11:56:23 +0200

    Revert "sm8450-common: Switch to AIDL thermal HAL"
    android.hardware.thermal-service.qti was declared as non-existant
    
    This reverts commit 5c99b2a649720ffbb2b4dca31f742d78955b0f63.

--- common.mk
+++ common.mk
@@ -538,7 +538,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.qti
+    android.hardware.thermal@2.0-service.qti-v2
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
