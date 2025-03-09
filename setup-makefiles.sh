daba44d7cf0a5539ccb637958e6138b9cc597ac4
Rinigus <rinigus.git@gmail.com>
2025-03-09 10:24:38 +0200

    sm8450-common: revise qcc imports
    Some QCC libraries were missing; vendor.qti.hardware.qccvndhal is not defined in stock manifest

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -60,7 +60,6 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.qti.hardware.qccsyshal@1.0 | \
             vendor.qti.hardware.qccsyshal@1.1 | \
             vendor.qti.hardware.qccsyshal@1.2 | \
-            vendor.qti.hardware.qccvndhal@1.0 | \
             vendor.qti.hardware.wifidisplaysession@1.0)
             echo "$1_vendor"
             ;;
