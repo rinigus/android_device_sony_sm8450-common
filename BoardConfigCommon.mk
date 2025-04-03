87ee560f8a45e31eb1c3ff3474f1b1efe7a564ed
Rinigus <rinigus.git@gmail.com>
2025-04-03 18:33:18 +0300

    sm8450: Switch to userdebug build

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,6 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive avc.debug=0 audit=0
 
 # removed: not in pdx223
 # BOARD_KERNEL_CMDLINE += nosoftlockup
